1d5b29867e84f4a53a11e87caa362cc070e3a993
Magnus Pladsen <93226629+MagnusPladsen@users.noreply.github.com>
2022-09-29 10:10:44 +0200

    Done with first delivery in Programming Foundations

---
+++ Programming Foundations/magnusPladsen-pf-mal.js
@@ -0,0 +1,27 @@
+// question 1
+const name = "Magnus Pladsen";
+let age = 25;
+let alive = true;
+
+// question 2
+const firstName = "Magnus";
+const lastName = "Pladsen";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// question 3
+const frogType = typeof "Frog";
+console.log(`The type of frog is ${frogType}`);
+
+// question 4
+let orderHasShipped = false;
+if (orderHasShipped === true) {
+  console.log("The order shipped");
+} else {
+  console.log("The order did not ship");
+}
+
+// question 5
+for (let i = 7; i <= 13; i++) {
+  console.log(i);
+}
